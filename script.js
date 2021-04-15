2b91a9e46d0d6594dfb869fbc7c1b9a64cfe6485
JuukoWK <71335087+JuukoWK@users.noreply.github.com>
2021-04-15 16:40:37 -0400

    Search Bar Attempt1

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ const constraints = {
     },
   }
 };
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+        console.log("Something went wrong!");
+      });
+  }
+  
+  function stop(e) {
+    var stream = video.srcObject;
+    var tracks = stream.getTracks();
+
+    for (var i = 0; i < tracks.length; i++) {
+      var track = tracks[i];
+      track.stop();
+    }
+
+    video.srcObject = null;
+  }
