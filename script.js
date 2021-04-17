8095f21546954904646f8cf3c2cf04cd9068e8a9
JuukoWK <71335087+JuukoWK@users.noreply.github.com>
2021-04-17 13:57:29 -0400

    SaerchBox update

--- script.js
+++ script.js
@@ -1,37 +1,116 @@
-var video = document.querySelector("#videoElement");
-const constraints = {
-  video: {
-    width: {
-      min: 1280,
-      ideal: 1920,
-      max: 2560,
-    },
-    height: {
-      min: 720,
-      ideal: 1080,
-      max: 1440
-    },
-  }
-};
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const result = document.getElementById('result')
 
-if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia(constraints)
-      .then(function (stream) {
-        video.srcObject = stream;
-      })
-      .catch(function (err0r) {
-        console.log("Something went wrong!");
-      });
-  }
-  
-  function stop(e) {
-    var stream = video.srcObject;
-    var tracks = stream.getTracks();
 
-    for (var i = 0; i < tracks.length; i++) {
-      var track = tracks[i];
-      track.stop();
+/// api URL ///
+const apiURL = 'https://api.lyrics.ovh';
+
+
+/// adding event listener in form
+
+form.addEventListener('submit', e=> {
+    e.preventDefault();
+    searchValue = search.value.trim()
+
+    if(!searchValue){
+        alert("There is nothing to search")
     }
+    else{ 
+        searchSong(searchValue)
+    }
+})
 
-    video.srcObject = null;
+
+//search song 
+async function searchSong(searchValue){
+    const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`)
+    const data = await searchResult.json();
+
+    // console.log(finaldata)
+    showData(data)
+}
+
+//display final result in DO
+function showData(data){
+  
+    result.innerHTML = `
+    <ul class="song-list">
+      ${data.data
+        .map(song=> `<li>
+                    <div>
+                        <strong>${song.artist.name}</strong> -${song.title} 
+                    </div>
+                    <span data-artist="${song.artist.name}" data-songtitle="${song.title}"> Play </span>
+                </li>`
+        )
+        .join('')}
+    </ul>
+  `;
+}
+
+
+//event listener in get lyrics button
+result.addEventListener('click', e=>{
+    const clickedElement = e.target;
+
+    //checking clicked elemet is button or not
+    if (clickedElement.tagName === 'SPAN'){
+        const artist = clickedElement.getAttribute('data-artist');
+        const songTitle = clickedElement.getAttribute('data-songtitle');
+        
+        getLyrics(artist, songTitle)
+    }
+})
+
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+  
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+  
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <p>${lyrics}</p>`;
+  
   }
+
+
+// load video player
+var video = document.querySelector("#videoElement");
+    const constraints = {
+      video: {
+        width: {
+          min: 1280,
+          ideal: 1920,
+          max: 2560,
+        },
+        height: {
+          min: 720,
+          ideal: 1080,
+          max: 1440
+        },
+      }
+    };
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (stream) {
+          video.srcObject = stream;
+        })
+        .catch(function (err0r) {
+          console.log("Something went wrong!");
+        });
+    }
+    
+    function stop(e) {
+      var stream = video.srcObject;
+      var tracks = stream.getTracks();
+
+      for (var i = 0; i < tracks.length; i++) {
+        var track = tracks[i];
+        track.stop();
+      }
+
+      video.srcObject = null;
+    }
